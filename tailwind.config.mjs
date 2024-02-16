2de9138d9d20d1fc19ce2f60b3c9e91febab0d5e
sangkukbaee <sangkukbae@gmail.com>
2024-02-16 21:09:21 +0900

    Remove font files and update Tailwind CSS and global styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,24 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontSize: {
+				'header-2': 'clamp(2rem, 4vw + 1rem, 3.5rem)'
+			}
+		}
 	},
-	plugins: [],
+	plugins: [
+		function ({ addUtilities }) {
+			const newUtilities = {
+				'.clip-path-undercut': {
+					clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0 100%)'
+				},
+				'.clip-path-overcut': {
+					clipPath: 'polygon(0 0, 100% 0, 100% 97%, 0% 97%)'
+				}
+			}
+
+			addUtilities(newUtilities)
+		}
+	]
 }
